bc5f7c619b3edc96e5e184679fc3db24a8f7da08
JulesG10 <jules10avrg@gmail.com>
2023-03-22 22:45:23 +0100

    update build.bat && new BTP version (raylib 4.5 update)

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 if exist dist (
     rm -rf dist
 ) 
-pyinstaller dungeon.spec && rm -rf build && xcopy .\assets\ .\dist\dungeon\assets /E /H /C /I
+pyinstaller dungeon.spec && rm -rf build && xcopy .\assets\ .\dist\dungeon\assets /E /H /C /I && xcopy .\plugin\*.py .\dist\dungeon\plugin\ /Y
 pause
